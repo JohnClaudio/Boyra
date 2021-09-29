0020c6215ca16ccf58f94fe40ca93f88ba041d5b
Melissa Amorim <melissa18.mo@gmail.com>
2021-09-29 11:31:49 -0300

    Inclusão dos arquivos corretos

---
+++ backend/src/test/java/br/org/generation/boyra/BoyraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.boyra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
