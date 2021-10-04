2c52de18092dbdd09a6bbb6cbfdd73f179234d15
Melissa Amorim <melissa18.mo@gmail.com>
2021-10-04 10:45:04 -0300

    inclusão do método Post

--- backend/src/main/java/br/org/generation/boyra/controller/CategoriaController.java
+++ backend/src/main/java/br/org/generation/boyra/controller/CategoriaController.java
@@ -3,10 +3,13 @@ package br.org.generation.boyra.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +36,11 @@ public class CategoriaController {
 			.map(resp -> ResponseEntity.ok(resp))
 			.orElse(ResponseEntity.notFound().build());
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria (@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(Repository.save(categoria));
+	}
+
+	
 }
