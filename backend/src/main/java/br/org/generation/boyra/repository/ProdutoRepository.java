8f3e7536ef9a0b477caba93796c356cd36a03bad
Thalyta Campos <52604345+samilathalyta@users.noreply.github.com>
2021-10-05 11:28:11 -0300

    Update ProdutoRepository.java

--- backend/src/main/java/br/org/generation/boyra/repository/ProdutoRepository.java
+++ backend/src/main/java/br/org/generation/boyra/repository/ProdutoRepository.java
@@ -1,2 +1,12 @@
-package br.org.generation.boyra.repository;public interface ProdutoRepository {
+package br.org.generation.boyra.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.org.generation.boyra.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository <Produto, Long>{
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	
 }
