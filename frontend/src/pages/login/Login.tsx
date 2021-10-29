8df1d15e77cfafa418922fc42610885c652a8d81
Thalyta Campos <52604345+samilathalyta@users.noreply.github.com>
2021-10-29 20:36:38 -0300

    Update Login.tsx

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -30,7 +30,7 @@ function Login() {
                     </Box>
                 </Box>
             </Grid>
-            <Grid xs={6} className='imagem'>
+            <Grid xs={6} >
                 
             </Grid>
 
