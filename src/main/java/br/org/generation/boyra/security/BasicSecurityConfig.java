25ad33459456a67399dd9ccbcfa08047e3a93fb3
João Claudio <61328786+JohnClaudio@users.noreply.github.com>
2021-11-11 14:17:08 -0300

    Update BasicSecurityConfig.java

--- src/main/java/br/org/generation/boyra/security/BasicSecurityConfig.java
+++ src/main/java/br/org/generation/boyra/security/BasicSecurityConfig.java
@@ -41,7 +41,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	http.authorizeRequests()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/produtos").permitAll()
+		.antMatchers("/produtos").permitAll()
 		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
